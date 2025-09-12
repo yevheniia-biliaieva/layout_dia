45d185ee6886b0b6f400f0b2a826046e8bf881b1
Yevheniia <biliaieva99@gmail.com>
2025-09-12 15:34:19 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,60 @@
 'use strict';
+// #region menu
+const menuButtons = document.querySelectorAll('.top-bar__menu-button');
+const sideMenu = document.querySelector('.page__menu');
+
+menuButtons.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const isActive = sideMenu.classList.toggle('is-open');
+    document
+      .querySelector('.page__body')
+      .classList.toggle('no-scroll', isActive);
+    menuButtons.forEach((b) => b.classList.toggle('is-active', isActive));
+  });
+});
+
+// #endregion
+
+// #region slider
+const slider = document.querySelector('.slider');
+const leftArrow = document.querySelector('.slider__arrow--left');
+const rightArrow = document.querySelector('.slider__arrow--right');
+
+let currentIndex = 1;
+
+function updateSlider() {
+  slider.className = `header__slider slider slider--${currentIndex}`;
+}
+
+leftArrow.addEventListener('click', (e) => {
+  e.preventDefault();
+  currentIndex--;
+  if (currentIndex < 1) {
+    currentIndex = 3;
+  }
+  updateSlider();
+});
+
+rightArrow.addEventListener('click', (e) => {
+  e.preventDefault();
+  currentIndex++;
+  if (currentIndex > 3) {
+    currentIndex = 1;
+  }
+  updateSlider();
+});
+
+// #endregion
+
+// #region textarea
+const textareas = document.querySelectorAll('textarea.contact-us__input');
+
+textareas.forEach((el) => {
+  el.setAttribute('style', 'height:' + el.scrollHeight + 'px;overflow:hidden;');
+  el.addEventListener('input', () => {
+    el.style.height = '41px';
+    el.style.height = el.scrollHeight + 'px';
+  });
+});
+
+// #endregion
